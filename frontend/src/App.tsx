6cf8f3811f795dcea250cedbd012827eb9e6370a
Vandana <vvandanapatidar992@gmail.com>
2026-08-10 09:28:04 +0530

    feat: make route pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-// import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage } from './components/Home';
+import { LandingPage } from './components/Landing';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
+import Layout from './components/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />, // Layout provides Header and Outlet
+    children: [
+      { index: true, element: <LandingPage /> },
+      { path: 'login', element: <LoginPage /> },
+      { path: 'signup', element: <SignupPage /> },
+      { path: 'home', element: <HomePage /> },
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-      <div>hello</div>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
+
